3f8adabb49a49fd3e99265222c943bd90da6cfea
Piotr Skrobol <taboritis@gmail.com>
2024-07-25 11:59:03 +0200

    Add BasicAuth middleware for /upload and /download routes

---
+++ config/basic-auth.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'username' => env('BASIC_AUTH_USERNAME', 'adrian'),
+    'password' => env('BASIC_AUTH_PASSWORD', 'lubie-placki'),
+];
--- tests/Feature/DownloadFileTest.php
+++ tests/Feature/DownloadFileTest.php
@@ -13,6 +13,10 @@ class DownloadFileTest extends TestCase
     #[Test]
     public function it_can_download_last_file(): void
     {
+        $this->withHeaders([
+            'Authorization' => 'Basic ' . base64_encode('adrian:lubie-placki'),
+        ]);
+
         File::copy(
             base_path('tests/Feature/fixtures/lastImage.jpeg'),
             storage_path('images/lastImage.jpg')
@@ -20,5 +24,33 @@ class DownloadFileTest extends TestCase
 
         $this->get('/images/lastImage/download')
             ->assertStatus(200);
+    }
+
+    #[Test]
+    public function it_return_401_when_unauthenticated(): void
+    {
+        File::copy(
+            base_path('tests/Feature/fixtures/lastImage.jpeg'),
+            storage_path('images/lastImage.jpg')
+        );
+
+        $this->get('/images/lastImage/download')
+            ->assertStatus(401);
+    }
+
+    #[Test]
+    public function it_returns_404_as_user_is_unauthorized(): void
+    {
+        $this->withHeaders([
+            'Authorization' => 'Basic ' . base64_encode('adrian:wrong-password'),
+        ]);
+
+        File::copy(
+            base_path('tests/Feature/fixtures/lastImage.jpeg'),
+            storage_path('images/lastImage.jpg')
+        );
+
+        $this->get('/images/lastImage/download')
+            ->assertStatus(403);
     }
 }
